e67c26c228d638c6270459b69996d9d00d5535b0
Ice Windy <40712698+IceWindy233@users.noreply.github.com>
2022-04-19 13:27:11 +0800

    更新结构体。

--- Base.h
+++ Base.h
@@ -1,27 +1,29 @@
-#include <stdlib.h>
 
-enum Gender{
-    man = 1, woman
-};
+enum Sex { male, female };    //性别枚举( 男 女 )
 
 typedef struct undergraduate
-{ //本科生基本资料数据（学号、姓名、性别、专业、班级）
-    int sun;
-    char name[4];
-    enum Gender gender;
-    char specialized[10];
-    char class[12];
+{
+    int num;//学号
+    char name[15];//姓名
+    enum Sex sex;//性别
+    char major[30];//专业
+    char banji[3];//班级
+    /*成绩*/
+    int score[12];//高数成绩、C语言成绩、英语成绩、总成绩、班级排名、校级排名
     struct undergraduate *next;
 } UND;
 
 typedef struct graduate
-{ //研究生基本资料数据（学号、姓名、性别、专业、班级、研究方向、导师）
-    int sun;
-    char name[4];
-    enum Gender gender;
-    char specialized[10];
-    char class[10];
-    char direction[10];
-    char mentor[4];
+{
+    int num;//学号
+    char name[10];//姓名
+    enum Sex sex;//性别
+    char major[20];//专业
+    int Class;//班级
+    char reserch[20];//研究方向
+    char tname[10];//导师名字
+    int score[3]; //综合成绩、论文成绩、总成绩
+    int classrank;//班级排名
+    int allrank;//校级排名
     struct graduate *next;
 } GRA;
